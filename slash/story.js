fbb1de23c435001d2a84c319e755a5556fde0222
Thomas Etter <tometter03@gmail.com>
2025-01-14 10:48:00 -0500

    New update

---
+++ slash/story.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "story",
+    description: "Creates a story based on the prompt that is given to the bot. Will output a voice note",
+    options: [], // Add options if needed
+    run: async (client, interaction, args) => {
+
+    }
+}
